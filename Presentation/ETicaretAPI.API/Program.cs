c6004e375ec869620fce0bf70060bda4a8018ab7
haticezzehra <haticezehraorhan@gmail.com>
2023-05-31 17:27:09 +0300

    CORS Policy

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -3,7 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddPersistenceServices();
-builder.Services.AddCors(options=>options.AddDefaultPolicy(policy=>policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()));
+builder.Services.AddCors(options=>options.AddDefaultPolicy(policy=>
+policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
+
 
 
 builder.Services.AddControllers();
